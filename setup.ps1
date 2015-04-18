d0520036d0fa80e41198937f1bda0f349e4f5078
John Walley <john@walley.org.uk>
2015-04-18 20:04:58 +0100

    Learning how Octopus.Client works

--- setup.ps1
+++ setup.ps1
@@ -7,18 +7,22 @@ $tentacleHome = "C:\Octopus\Tentacle"
 $tentacleConfigPath = "C:\Octopus\Tentacle\Tentacle.config"
 $appsPath = "C:\Octopus\Applications"
 
+&"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance $instanceName --stop --uninstall
 &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" delete-instance --instance $instanceName
+&"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance $instanceName --stop --uninstall
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" delete-instance --instance $instanceName
 
 &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" create-instance --instance $instanceName --config $serverConfigPath
 &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" configure --instance $instanceName --home $serverPath --storageMode "Embedded" --upgradeCheck "True" --upgradeCheckWithStatistics "True" --webAuthenticationMode "UsernamePassword" --webForceSSL "False" --webListenPrefixes "http://localhost:80/" --storageListenPort "10931" --commsListenPort "10943"
 &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" admin --instance $instanceName --username "Redgate" --password "Redg@te1" --wait "5000"
 &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" license --instance $instanceName --free
-&"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance $instanceName --install --reconfigure --start
 
+# Must do this before the service is started
 $thumbOut = &"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" show-thumbprint --instance $instanceName
 $thumbPrint = $thumbOut[$thumbOut.Length - 2]
 "Octopus Thumbprint : $thumbPrint"
+
+&"C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance $instanceName --install --reconfigure --start
 
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" create-instance --instance $instanceName --config $tentacleConfigPath --console
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" new-certificate --instance $instanceName --console
@@ -26,4 +30,4 @@ $thumbPrint = $thumbOut[$thumbOut.Length - 2]
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance $instanceName --app $appsPath --console
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance $instanceName --port "10933" --console
 &"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance $instanceName --trust $thumbPrint --console
-&"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance instanceName --install --start --console
+&"C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance $instanceName --install --start --console
---
+++ source/Program.cs
@@ -0,0 +1,161 @@
+﻿using System.Linq;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Octopus.Client;
+using Octopus.Client.Model;
+using Octopus.Platform.Model;
+using Octopus.Platform.Variables;
+
+namespace ProvisionOctopusDeploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpoint = new OctopusServerEndpoint("http://localhost/");
+            var repository = new OctopusRepository(endpoint);
+
+            repository.Users.SignIn(new LoginCommand {Username = "Redgate", Password = "Redg@te1"});
+
+            var integrationEnvironment = CreateEnvironment(repository, "Integration");
+            var uatEnvironment = CreateEnvironment(repository, "UAT");
+            var productionEnvironment = CreateEnvironment(repository, "Production");
+
+            var integrationMachine = CreateMachine(repository, integrationEnvironment, "Integration");
+            var uatMachine = CreateMachine(repository, uatEnvironment, "UAT");
+            var productionMachine = CreateMachine(repository, productionEnvironment, "Production");
+
+            var environments = new[] {integrationEnvironment, uatEnvironment, productionEnvironment};
+        
+            var projectGroup = CreateProjectGroup(repository);
+
+            var lifecycle = repository.Lifecycles.FindOne(l => l.Name == "Default Lifecycle");
+
+            var project = CreateProject(repository, projectGroup, lifecycle, "SimpleTalk");
+
+            CreateDeploymentProcess(repository, project);
+
+            CreateDashboard(repository, environments, project);
+
+            var template = repository.Client.Get<ActionTemplateResource>(repository.Client.RootDocument.Links["ActionTemplates"]);
+
+            string name;
+            string scriptBody;
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = webClient.DownloadString("https://raw.githubusercontent.com/OctopusDeploy/Library/master/step-templates/redgate-create-database-release.json");
+                var o = JObject.Parse(json);
+                name = (string) o["Name"];
+                var properties = o["Properties"];
+                scriptBody = (string)properties["Octopus.Action.Script.ScriptBody"];
+            }
+
+            var actionTemplateResource = new ActionTemplateResource
+            {
+                Name = name,
+                ActionType = "Octopus.Script",
+                Properties =
+                        {
+                            {SpecialVariables.Action.Script.ScriptBody, scriptBody}
+                        }
+            };
+
+
+            repository.Client.Post(repository.Client.RootDocument.Links["ActionTemplates"], actionTemplateResource);
+
+        }
+
+        private static void CreateDeploymentProcess(OctopusRepository repository, ProjectResource project)
+        {
+            var deploymentProcess = repository.DeploymentProcesses.Get(project.DeploymentProcessId);
+
+            var deploymentStepResource = new DeploymentStepResource
+            {
+                Name = "Step A",
+                Condition = DeploymentStepCondition.Success,
+                Properties =
+                {
+                    {"Octopus.Action.TargetRoles", "database"}
+                },
+                Actions =
+                {
+                    new DeploymentActionResource
+                    {
+                        ActionType = "Octopus.Script",
+                        Name = "Say Hello",
+                        Properties =
+                        {
+                            {SpecialVariables.Action.Script.ScriptBody, "Write-Host 'Hello'"}
+                        }
+                    }
+                }
+            };
+
+            deploymentProcess.Steps.Add(deploymentStepResource);
+
+            repository.DeploymentProcesses.Modify(deploymentProcess);
+        }
+
+        private static DashboardConfigurationResource CreateDashboard(OctopusRepository repository, IResource[] environments,
+            IResource project)
+        {
+            var dashboardConfiguration = repository.DashboardConfigurations.GetDashboardConfiguration();
+
+            dashboardConfiguration.IncludedEnvironmentIds = new ReferenceCollection(environments.Select(e => e.Id));
+            dashboardConfiguration.IncludedProjectIds = new ReferenceCollection(project.Id);
+            return dashboardConfiguration;
+        }
+
+        private static IResource CreateMachine(IOctopusRepository repository, IResource productionEnvironment, string name)
+        {
+            var machine = repository.Machines.Discover("localhost");
+
+            machine.Name = name;
+            machine.EnvironmentIds = new ReferenceCollection(productionEnvironment.Id);
+            machine.Roles.Add("database");
+
+            return repository.Machines.FindByName(name) ?? repository.Machines.Create(machine);
+        }
+
+        private static IResource CreateEnvironment(IOctopusRepository repository, string name)
+        {
+            return repository.Environments.FindByName(name) ??
+                   repository.Environments.Create(new EnvironmentResource {Name = name});
+        }
+
+        private static ProjectResource CreateProject(IOctopusRepository repository, IResource projectGroup,
+            IResource lifecycle, string name)
+        {
+            var project = repository.Projects.FindByName(name);
+
+            if (project != null)
+            {
+                repository.Projects.Delete(project);
+                Thread.Sleep(500);
+            }
+
+            var projectResource = new ProjectResource
+            {
+                Name = name,
+                ProjectGroupId = projectGroup.Id,
+                LifecycleId = lifecycle.Id
+            };
+
+            return repository.Projects.Create(projectResource);
+        }
+
+        private static ProjectGroupResource CreateProjectGroup(IOctopusRepository repository)
+        {
+            var projectGroupResource = new ProjectGroupResource
+            {
+                Name = "SimpleTalk",
+                Description = "Deploy SimpleTalk application and database schema"
+            };
+
+            var projectGroup = repository.ProjectGroups.FindByName("SimpleTalk") ??
+                               repository.ProjectGroups.Create(projectGroupResource);
+            return projectGroup;
+        }
+    }
+}
